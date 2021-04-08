60424c864aaf8aea6e819a1178ecc06906e2aab6
Josh <leonidus00@yahoo.com>
2021-04-07 20:16:58 -0500

    commit tree b37dea414f802ff6f5613fa4ed173975aecfc27b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+
+function saturdayFun(activity= 'roller-skate'){
+       return `This Saturday, I want to ${activity}!`
+
+  }   
+
+function mondayWork(task ='go to the office') {
+    
+    return `This Monday, I will ${task}.`
+    
+  
+}
+    function wrapAdjective(kind='*') {
+        return function (adjective ="special") {
+      return `You are ${kind}${adjective}${kind}!`;
+    }
+    
+        
+      
+        
+
+}
